71e273329af577036db7b51eddb8ec7fa8a934b3
lukeklotz <lukeklotz@gmail.com>
2025-04-15 12:54:12 -0700

    changed megaplanet title and started mobile styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import { createRoot } from 'react-dom/client';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+
+const artists = [
+  { name: "KEVIN NAGEL", url: "https://kevinnagel.bandcamp.com/album/echoes-from-tomorrow" },
+  { name: "MARSH CRANE", url: "https://sweb16.bandcamp.com/" },
+  { name: "ORCHID81", url: "https://orchid81.bandcamp.com/" },
+  { name: "MAGICDJ", url: "https://universalmagic.bandcamp.com/" },
+  { name: "CHUCKNESSA", url: "https://chucknessa.bandcamp.com/" },
+  { name: "MOONSIDE", url: "https://itsmoonside.bandcamp.com/" },
+  { name: "KOKIO", url: "https://kokio.bandcamp.com/album/mysticism-unmastered" },
+  { name: "MLTZR", url: "https://mltzr.bandcamp.com/" }
+];
 
 // Rotating Sphere component with responsive size and smooth rotation
 function RotatingSphere() {
@@ -28,7 +39,7 @@ function RotatingSphere() {
   });
 
   // Get dynamic radius based on window width
-  let radius = Math.max(.001, window.innerWidth / 900);
+  let radius = Math.max(.001, window.innerWidth / 600);
   
   if (radius < .7) {    //hacky way to check if we're on an iPhone....
     radius = Math.max(.001, window.innerWidth / 300);
@@ -42,66 +53,25 @@ function RotatingSphere() {
   );
 }
 
-// Rotating Box component (just for additional elements in your scene)
-function RotatingBoxTop() {
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.001; // Rotate around Y-axis
-      meshRef.current.rotation.x += 0.0001; // Slight rotation around X-axis
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} position={[-13, 8, -10]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="red" wireframe />
-    </mesh>
-  );
-}
-
-// Rotating Box component at the bottom
-function RotatingBoxBottom() {
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.001; // Rotate around Y-axis
-      meshRef.current.rotation.x += 0.0001; // Slight rotation around X-axis
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} position={[25, -15, -25]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="red" wireframe />
-    </mesh>
-  );
-}
-
 // Big Header Text Component
 function BigText() {
   return (
     <header className="mega-planet">
-      <h1>MEGAPLANET.COM</h1>
+      <h1>MEGAPLANET.WORLD</h1>
     </header>
   );
 }
 
 // Links to band pages
-function Names() {
+function Names({artists}) {
   return(
     <div className="names-container">
-       <a href='https://kevinnagel.bandcamp.com/album/echoes-from-tomorrow'><h3>KEVIN NAGEL</h3></a>
-       <a href='https://sweb16.bandcamp.com/'><h3>MARSH CRANE</h3></a>
-       <a href="https://orchid81.bandcamp.com/"><h3>ORCHID81</h3></a>
-       <a href='https://universalmagic.bandcamp.com/'><h3>MAGICDJ</h3></a>
-       <a href='https://chucknessa.bandcamp.com/'><h3>CHUCKNESSA</h3></a>
-       <a href='https://itsmoonside.bandcamp.com/'><h3>MOONSIDE</h3></a>
-       <a href='https://kokio.bandcamp.com/album/mysticism-unmastered'><h3>KOKIO</h3></a>
-       <a href='https://mltzr.bandcamp.com/'><h3>MLTZR</h3></a>
-    </div>
+      {artists.map((artist, index) => (
+        <a key={index} href={artist.url}>
+          <h3>{artist.name}</h3>
+        </a>
+    ))}
+</div>
   );
 }
 
@@ -109,7 +79,7 @@ function App() {
   return (
     <div className="canvas-container">
       <BigText />
-      <Names />
+      <Names artists={artists}/>
       <Canvas>
         <ambientLight intensity={1.0} />
         <pointLight position={[2, 5, 5]} />
